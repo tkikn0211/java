b33f7cde4c0389bbbc7e79417ea57d652500ccd0
Ken Takei <95737638+tkikn0211@users.noreply.github.com>
2022-04-23 23:56:17 +0900

    Chapter04

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str = "ken takei";
+		System.out.println(str);
+		str = "金は命より重い";
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
