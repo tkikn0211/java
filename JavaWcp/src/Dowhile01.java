0b05170ca1e9afc26013cec1588932b287d3e092
Ken Takei <95737638+tkikn0211@users.noreply.github.com>
2022-05-08 15:07:40 +0900

    chapter08

---
+++ JavaWcp/src/Dowhile01.java
@@ -0,0 +1,10 @@
+
+public class Dowhile01 {
+	public static void main (String[] args) {
+		int number = 1;
+		do {
+			number *= 2;
+			System.out.println("Dowhile01 = " + number);
+		} while (number < 50);
+	}
+}
