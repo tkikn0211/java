bd6b43c764e17d8301008747a3cf0927c9ae5ad5
Ken Takei <95737638+tkikn0211@users.noreply.github.com>
2022-05-23 19:52:00 +0900

    Q020

---
+++ JavaWcp/src/ECFtech/Q019.java
@@ -0,0 +1,13 @@
+package ECFtech;
+
+public class Q019 {
+	public static void main(String[] args) {
+		int n = 7;
+		
+		for(int i = 1; i <= n ; i++) {
+			System.out.print(i);
+			System.out.print(",");
+//			System.out.print(i + ",");まとめられる
+		}
+	}
+}
---
+++ JavaWcp/src/ECFtech/Q021.java
@@ -0,0 +1,19 @@
+package ECFtech;
+
+public class Q021 {
+	public static void main(String[] args) {
+		int n = 20;
+		
+		for(int i = 1; i <= n; i++) {
+			if(i % 3 == 0) {
+				System.out.print("?");
+			} else {
+				System.out.print(i);
+			}
+			
+			if(i != n) {
+				System.out.print(",");
+			}
+		}
+	}
+}
