7238ec2446c96ad771496ba57d7b9750dab05030
Ken Takei <95737638+tkikn0211@users.noreply.github.com>
2022-05-28 00:32:11 +0900

    Q034

--- JavaWcp/src/ECFtech/Q033.java
+++ JavaWcp/src/ECFtech/Q033.java
@@ -1,5 +1,22 @@
 package ECFtech;
 
 public class Q033 {
+	public static void main(String[] args) {
+		String[] a = { "abc", "abcd", "abcde", "xx", "yyy" };
 
+		String key = "abcde";
+
+		int i;
+		for (i = 0; i < a.length; i++) {
+			if (a[i].equals(key)) {
+				break;
+			}
+		}
+
+		if (i < a.length) {
+			System.out.println(i);
+		} else {
+			System.out.println("Not found");
+		}
+	}
 }
---
+++ JavaWcp/src/ECFtech/Q034.java
@@ -0,0 +1,34 @@
+package ECFtech;
+
+public class Q034 {
+//	public static void main(String[] args) {
+//		int[] a = { 68, 55, 72, 93, 87 };
+//		
+//		int minValue = a[0];
+//		int minIndex = 0;
+//		
+//		for(int i = 1; i < a.length; i++) {
+//			if(minValue > a[i]) {
+//				minValue = a[i];
+//				minIndex = i;
+//			}
+//		}
+//		
+//		System.out.println(minValue);
+//		System.out.println(minIndex);
+//	}
+	
+	public static void main(String[]args) {
+		int[] a = { 68, 55, 72, 93, 87 };
+		
+		int minIndex = 0;
+		
+		for(int i = 1; i < a.length; i++) {
+			if(a[minIndex] > a[i]) {
+				minIndex = i;
+			}
+		}
+		System.out.println(a[minIndex]);
+		System.out.println(minIndex);
+	}
+}
