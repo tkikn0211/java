cee16535803a04096c436a65a14acf27f34edcd1
Ken Takei <95737638+tkikn0211@users.noreply.github.com>
2022-06-23 23:48:59 +0900

    更新

--- JavaWcp/src/JAVA_practice/Qsannkakkei.java
+++ JavaWcp/src/JAVA_practice/Qsannkakkei.java
@@ -2,8 +2,7 @@ package JAVA_practice;
 
 public class Qsannkakkei {
 	public static void main(String[] args) {
-
-		for (int i = 0; i < 5; i++) {
+		for(int i = 0; i < 5; i++) {
 			
 			for(int j = 0; j < 5 - (i + 1); j++) {
 				System.out.print(" ");
---
+++ スッキリわかるJAVA/src/スッキリわかるJAVA/Code610Api.java
@@ -0,0 +1,12 @@
+package スッキリわかるJAVA;
+
+public class Code610Api {
+	public static void main(String[] args) {
+		int [] heights = {172, 149, 152, 191, 155};
+		java.util.Arrays.sort(heights);
+		
+		for(int h : heights) {
+			System.out.println(h);
+		}
+	}
+}
