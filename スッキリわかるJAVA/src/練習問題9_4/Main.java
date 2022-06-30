8c2a927f5f306e68c1657941bad7095189e9c932
Ken Takei <95737638+tkikn0211@users.noreply.github.com>
2022-06-30 19:34:32 +0900

    更新

---
+++ スッキリわかるJAVA/src/練習問題9_4/Main.java
@@ -0,0 +1,20 @@
+package 練習問題9_4;
+
+public class Main {
+	public static void heal(int hp) {
+		hp += 10;
+	}
+	
+	public static void heal(Thief thief) {
+		thief.hp += 10;
+	}
+	
+	public static void main(String [] args) {
+		int baseHp = 25;
+		Thief t = new Thief("アサカ", baseHp);
+		heal(baseHp);
+		System.out.println(baseHp + ":" + t.hp);
+		heal(t);
+		System.out.println(baseHp + ":" + t.hp);
+	}
+}
--- スッキリわかるJAVA/src/練習問題9_4/Thief.java
+++ スッキリわかるJAVA/src/練習問題9_4/Thief.java
@@ -1,5 +1,21 @@
 package 練習問題9_4;
 
 public class Thief {
+	String name;
+	int hp;
+	int mp;
 
+	public Thief(String name, int hp, int mp) {
+		this.name = name;
+		this.hp = hp;
+		this.mp = mp;
+	}
+	
+	public Thief(String name, int hp) {
+		this(name, hp, 5);
+	}
+	
+	public Thief(String name) {
+		this(name, 40);
+	}
 }
